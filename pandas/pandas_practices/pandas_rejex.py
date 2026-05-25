444f62ac8132ce2da8cc327246ae51f74eda1289
DurgeshKhairnar <2002durgeshk@gmail.com>
2026-05-25 09:30:28 +0530

    regex and practise question

--- pandas/pandas_practices/pandas_rejex.py
+++ pandas/pandas_practices/pandas_rejex.py
@@ -83,4 +83,78 @@ print("Find invalid emails")
 for i in df["Email"]:
  if not re.search(r"^[\w\.-]+@[\w\.-]+\.\w+$",i):
   print(i)
-    
+
+
+
+# df["Phone"] = pd.to_numeric(
+#     df["Phone"],
+#     errors="coerce"
+# )
+
+# convert the NaN to 0
+
+# df['Phone'] = df['Phone'].replace(np.nan,1234567890)
+# print(df)
+
+# print("Find valid 10-digit phone numbers") 
+# for i in df["Phone"]:
+#  if not re.findall(r"\d{10}",i):
+#   print(i)
+
+print(df)
+
+print("Find cities containing numbers")
+for i in df['City']:
+ if re.findall('\d',i):
+  print(i)
+
+print("Find cities containing special characters")  
+for i in df["City"]:
+    if re.findall(r"[^a-zA-Z0-9]",i):
+      print(i)
+
+# print("Replace special characters from Name column")
+# df['Name']  = df["Name"].replace(r"[^a-zA-Z0-9]"," ")
+# print(df["Name"]) 
+
+# print("Extract only numbers from Phone column")
+# for i in df["Phone"]:
+#   if i in r"\d":
+#     print(i)
+
+print("Convert Salary to integer")
+df["Salary"] = pd.to_numeric(
+    df["Salary"],
+    errors="coerce"
+)
+print(df["Salary"].dtype)
+
+
+print("Find names starting with A" )
+print(df[df['Name'].str.startswith("A")])
+
+print("Find emails ending with gmail.com")
+print(df[df['Email'].str.endswith("gmail.com")])
+
+print("Find rows where Salary > 55000")
+print(df[df['Salary'] > 55000])
+
+print("Create new column:")
+df['Bonus'] = df['Salary'] * 0.1
+print(df)
+
+print("Sort by Salary descending")
+df["Salary"].sort_values(ascending=False)
+print(df)
+
+print("Group by City and find average Salary")
+print(df.groupby("City")['Salary'].mean())
+
+# print("Remove duplicate rows")
+# df.drop_duplicates()
+
+# print("""Rename column "Salary" to "Income""")
+# df.rename([df["Salary"] = "Income"])
+
+print("Check data types")
+print(df.dtypes)
