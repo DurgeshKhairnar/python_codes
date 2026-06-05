8d7e381f8f4139ec225ab5b41ec18e0e2e030ca1
DurgeshKhairnar <2002durgeshk@gmail.com>
2026-06-05 23:49:08 +0530

    python pra

---
+++ python/practices.py
@@ -0,0 +1,34 @@
+
+
+# for i in range(1,101):
+#     print(i,end=" ")
+
+
+# for i in range(1,51):
+#     if i % 2 == 0:
+#         print(i,end=" ")
+
+
+# for i in range(100,0,-1):
+#     if i % 2 == 1:
+#         print(i,end=" ")
+
+# sum = 0
+# for i in range(1,21):
+#     sum += i
+
+# print(f"sum of 20 num :{sum}")
+
+# num = 1234
+# temp = 0
+# while num > 0:
+#     rev = num % 10
+#     temp = temp * 10 + rev
+#     num = num // 10
+
+# print(temp)    
+
+name = "durgesh"
+
+for i in name:
+    print(name[:5])
